c64716abb5c1927104646da3481b36471b92f671
kohe0115 <vainsky.kou@gmail.com>
2024-05-08 23:11:42 +0900

    デザイン仮作成

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,118 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="rapper">
+    <div class="side__menu">
+      <div class="side__menu--header">
+        vue-chat
+      </div>
+      <div class="side__menu--contents">
+        <ul class="group__lists">
+          <li class="group__list"><a href="#">aaaaa</a></li>
+          <li class="group__list"><a href="#">bbbbb</a></li>
+          <li class="group__list"><a href="#">ccccc</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="main__contents">
+      <div class="main__contents--header">
+        <div class="main__contents--header__contents">
+          <div class="main__contents--header__current__group__name">
+            aaaaa
+          </div>
+          <div class="main__contents--header__login__user">
+            <div class="main__contents--header__login__user--image">
+              <img src="https://nekogazou.com/wp-content/uploads/2013/07/0142.jpg" class="avatar__image" alt="">
+            </div>
+            <div class="main__contents--header__login__user--name">
+              sample test
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="main__contents--content">
+        <div class="chat__contents">
+          <div class="chat__content">
+            <div class="chat__content--image">
+              <img src="https://nekogazou.com/wp-content/uploads/2013/07/0142.jpg" class="avatar__image" alt="">
+            </div>
+            <div class="chat__content--message">
+              <p>test</p>
+            </div>
+          </div>
+
+          <div class="chat__content--login__user">
+            <div class="chat__content--message--login__user">
+              <p>testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
+            </div>
+            <div class="chat__content--image">
+              <img src="https://nekogazou.com/wp-content/uploads/2013/07/0142.jpg" class="avatar__image" alt="">
+            </div>
+          </div>
+
+          <div class="chat__content--login__user">
+            <div class="chat__content--message--login__user">
+              <p>testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
+            </div>
+            <div class="chat__content--image">
+              <img src="https://nekogazou.com/wp-content/uploads/2013/07/0142.jpg" class="avatar__image" alt="">
+            </div>
+          </div>
+
+          <div class="chat__content">
+            <div class="chat__content--image">
+              <img src="https://nekogazou.com/wp-content/uploads/2013/07/0142.jpg" class="avatar__image" alt="">
+            </div>
+            <div class="chat__content--message">
+              <p>test</p>
+            </div>
+          </div>
+
+          <div class="chat__content--login__user">
+            <div class="chat__content--message--login__user">
+              <p>testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
+            </div>
+            <div class="chat__content--image">
+              <img src="https://nekogazou.com/wp-content/uploads/2013/07/0142.jpg" class="avatar__image" alt="">
+            </div>
+          </div>
+
+          <div class="chat__content--login__user">
+            <div class="chat__content--message--login__user">
+              <p>testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
+            </div>
+            <div class="chat__content--image">
+              <img src="https://nekogazou.com/wp-content/uploads/2013/07/0142.jpg" class="avatar__image" alt="">
+            </div>
+          </div>
+
+          <div class="chat__content">
+            <div class="chat__content--image">
+              <img src="https://nekogazou.com/wp-content/uploads/2013/07/0142.jpg" class="avatar__image" alt="">
+            </div>
+            <div class="chat__content--message">
+              <p>test</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="chat__form__contents">
+          <input class="chat__form__content--input" name="">
+          <button class="chat__form__content--btn">submit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import 'normalize.css';
+import './assets/style/common.css'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+require("./assets/scss/_prepends.scss");
 
 createApp(App).mount('#app')
